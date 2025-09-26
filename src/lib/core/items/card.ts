46bcb2cbc5f75c54ae04a86ad87bd05fcf1074e8
tbollen <thomas.bollen97@gmail.com>
2025-09-26 22:33:07 +0200

    create new class for individual cards

---
+++ src/lib/core/items/card.ts
@@ -0,0 +1,93 @@
+// Styling Types
+import { type CardStyleOptions } from '$lib/utils/types/style';
+import { type CardStylePreset, defaultCardStyle } from '$lib/core/items/cardStylePresets';
+import { type IsCardType } from '$lib/modules/cardTypes';
+
+// Basic Types
+import { type Prefixed_UUID } from '$lib/utils/uuid';
+
+// Importing mechanics for various systems (dynamically)
+import { gameCardSystems } from '$lib/system/gameSystems';
+type System = keyof typeof gameCardSystems; // e.g., 'arcane-rift', 'dnd5e', etc.
+type CardMechanics = { [K in keyof typeof gameCardSystems]?: (typeof gameCardSystems)[K] };
+// End of Type Magic
+
+// Basic Card fields
+export type CardFields = {
+	name?: string;
+	description?: string;
+};
+
+export const fallbackCardInfo: Partial<Card> = {
+	name: 'New Card',
+	type: 'Card',
+	description: 'Card Description',
+	image: {
+		rotation: 0,
+		scale: 100,
+		x_offset: 0,
+		y_offset: 0
+	},
+	stylePreset: 'default',
+	style: defaultCardStyle
+};
+
+export class Card {
+	// ID and db info
+	creatorId: Prefixed_UUID<'user'> | null = null;
+	createdAt: Date;
+	updatedAt: Date;
+	// Main info
+	name: string;
+	type: IsCardType;
+	subtitle?: string;
+	icon?: string;
+	description: string;
+	image: {
+		url?: string;
+		encodedImage?: string;
+		name?: string;
+		alt?: string;
+		rotation: number;
+		scale: number;
+		x_offset: number;
+		y_offset: number;
+	} = {
+		// Defaults for image
+		rotation: 0,
+		scale: 100,
+		x_offset: 0,
+		y_offset: 0
+	};
+	// Card styling
+	stylePreset: CardStylePreset = 'default';
+	style: CardStyleOptions = defaultCardStyle;
+	// Mechanics
+	systems: System[]; // Can have 0, 1 or more compatible systems
+	mechanics: CardMechanics; // Object containing mechanics per available system
+
+	// Constructor to initialize the card with default values
+	constructor(_card?: Partial<Card>) {
+		let _cardReference: Partial<Card> = fallbackCardInfo;
+		if (_card) {
+			_cardReference = { ..._cardReference, ..._card };
+		}
+		this.creatorId = _cardReference.creatorId ?? null;
+		this.createdAt = _cardReference.createdAt ?? new Date();
+		this.updatedAt = _cardReference.updatedAt ?? new Date();
+		this.name = _cardReference.name ?? fallbackCardInfo.name!;
+		this.type = _cardReference.type ?? fallbackCardInfo.type!;
+		this.subtitle = _cardReference.subtitle ?? undefined;
+		this.icon = _cardReference.icon ?? undefined;
+		this.description = _cardReference.description ?? fallbackCardInfo.description!;
+		this.image = {
+			...this.image,
+			...(_cardReference.image ?? {})
+		};
+		this.stylePreset = _cardReference.stylePreset ?? fallbackCardInfo.stylePreset!;
+		this.style = { ...defaultCardStyle, ...(_cardReference.style ?? {}) };
+		// Set mechanics based on system
+		this.systems = _cardReference.systems ?? [];
+		this.mechanics = { ...(_cardReference.mechanics ?? {}) };
+	}
+}
