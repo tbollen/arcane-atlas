a6a99d4edcf5994955e9593e3db9d193344bd101
tbollen <thomas.bollen97@gmail.com>
2025-09-26 12:27:56 +0200

    Split card Style types and presets

--- src/lib/core/presets/cardStylePresets.ts
+++ src/lib/core/presets/cardStylePresets.ts
@@ -1,38 +1,5 @@
-import { type AvailableFonts } from './fonts';
+import { type CardStyleOptions } from '$lib/utils/types/style';
 import { colorScheme } from '$lib/styles/colorScheme';
-
-// Color Types
-export type RGB = `rgb(${number}, ${number}, ${number})`;
-export type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
-export type HEX = `#${string}`;
-export type HSL = `hsl(${number}, ${number}%, ${number}%)`;
-export type HSLA = `hsla(${number}, ${number}%, ${number}%, ${number})`;
-export type Color = RGB | RGBA | HEX | HSL | HSLA;
-export type cssVariable = `var(--${string})`;
-
-export type CardStyleOptions = {
-	color: {
-		accent: Color;
-		icon: Color;
-		text: Color;
-		background: Color;
-		cardBorder: Color;
-		imageBackground: Color;
-	};
-	font: {
-		name: AvailableFonts;
-		subtitle: AvailableFonts;
-		accents: AvailableFonts;
-		text: AvailableFonts;
-	};
-	fontsize: {
-		text: number;
-		name: number;
-		subtitle: number;
-		check: number;
-		icon: number;
-	};
-};
 
 // Style Presets
 export const defaultCardStyle: CardStyleOptions = {
