6cba154ede731f0cc027feaf56ec7b28c418b5e7
tbollen <thomas.bollen97@gmail.com>
2025-09-26 22:42:26 +0200

    Populate arcane-rift card mechanics

--- src/lib/system/ArcaneRift/cards.ts
+++ src/lib/system/ArcaneRift/cards.ts
@@ -2,6 +2,22 @@
 
 export class AR_Card {}
 
+type Field = {
+	label: string;
+	value: string;
+};
+
+type Aspect = {
+	short: string;
+	description: string;
+};
+
 export type ArcaneRiftCard = {
 	// Arcane Rift specific mechanics here
+	aspects: Aspect[];
+	fields: Field[];
+	check?: {
+		characteristic: string;
+		skill: string;
+	};
 };
