7e35fde72219c4d690127b01f5fc042e163051af
tbollen <thomas.bollen97@gmail.com>
2025-09-26 22:32:36 +0200

    create types for specific systems

--- src/lib/system/ArcaneRift/cards.ts
+++ src/lib/system/ArcaneRift/cards.ts
@@ -1,3 +1,7 @@
 // Basic Types to use in the system
 
 export class AR_Card {}
+
+export type ArcaneRiftCard = {
+	// Arcane Rift specific mechanics here
+};
--- src/lib/system/gameSystems.ts
+++ src/lib/system/gameSystems.ts
@@ -1,1 +1,11 @@
-export const availableGameSystems = ['arcane-rift', 'dnd5e'] as const;
+export const availableGameSystems = ['arcane-rift', 'dnd5e'];
+
+// Get Item Mechanics from each system
+// ADD MORE WHEN MORE SYSTEMS ARE ADDED
+import { type ArcaneRiftCard } from '$lib/system/ArcaneRift/cards';
+
+export const gameCardSystems = {
+	undefined: {},
+	'arcane-rift': {} as ArcaneRiftCard
+	// ADD MORE WHEN MORE SYSTEMS ARE ADDED
+} as const;
