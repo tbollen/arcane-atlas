a90a01ed20ea05394856a5b3a2d0f742f3c4a0c3
tbollen <thomas.bollen97@gmail.com>
2025-10-01 09:14:55 +0200

    finish better auth init

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,7 @@
-// export { handle } from './auth';
+import { auth } from '$server/auth';
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { building } from '$app/environment';
+
+export async function handle({ event, resolve }) {
+	return svelteKitHandler({ event, resolve, auth, building });
+}
