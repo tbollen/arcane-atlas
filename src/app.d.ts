8abfc5e8fb4df5bb7cb3541ab336e336836e1bb4
tbollen <thomas.bollen97@gmail.com>
2025-10-01 16:02:55 +0200

    fixed hooks and auth types

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,12 +2,15 @@
 // for information about these interfaces
 
 import type { AuthRequest } from 'better-auth';
+import type { Session, User } from 'better-auth';
 
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			auth: AuthRequest;
+			session?: Session;
+			user?: User;
 		}
 		// interface PageData {}
 		// interface PageState {}
