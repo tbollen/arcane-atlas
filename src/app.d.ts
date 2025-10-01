5882f55ed199bf34b019383f1d108d605204eab9
tbollen <thomas.bollen97@gmail.com>
2025-10-01 15:49:45 +0200

    fix session and locals issues in IDE

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { AuthRequest } from 'better-auth';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: AuthRequest;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
