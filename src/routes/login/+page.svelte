89f8e3e77a2eacb53822f4a54d68be918787eab7
tbollen <thomas.bollen97@gmail.com>
2025-10-02 09:17:35 +0200

    Updated routes and created form actions

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,64 +3,50 @@
 	import { authClient } from '$lib/utils/auth/auth-client';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
-
-	let nameInput = '';
-	let emailInput = '';
-	let passwordInput = '';
+	import { enhance } from '$app/forms';
 
 	let session: any;
+
+	// +page.server.ts form actions return 'form' as an object with the form data
+	export let form;
 
 	onMount(async () => {
 		session = await authClient.getSession();
 		if (session && session?.data?.user) goto(`${base}/account`);
 	});
-
-	async function signUp() {
-		try {
-			await authClient.signUp.email({
-				email: emailInput,
-				password: passwordInput,
-				name: nameInput
-			});
-		} catch (error) {
-			console.error(error);
-			alert(error);
-		}
-	}
-
-	async function signIn() {
-		try {
-			await authClient.signIn.email({
-				email: emailInput,
-				password: passwordInput
-			});
-			goto(`${base}/account`);
-		} catch (error) {
-			console.error(error);
-			alert(error);
-		}
-	}
 </script>
 
 <main>
+	{#if form?.message}
+		<p>{form.success ? 'Success' : 'Error'}: {form.message}</p>
+	{/if}
+	<!-- TEST -->
+	<form method="POST" use:enhance>
+		<label for="email">Email</label>
+		<input type="email" name="email" />
+		<label for="password">Password</label>
+		<input type="password" name="password" />
+		<button formaction="?/signIn" type="submit">Sign In</button>
+	</form>
+	<!--  -->
 	<h1>Sign In</h1>
-	<form on:submit={signIn}>
+	<form>
 		<label for="emailInput">Email</label>
-		<input type="email" bind:value={emailInput} required />
+		<input type="email" required />
 		<label for="passwordInput">Password</label>
-		<input type="password" bind:value={passwordInput} required />
+		<input type="password" required />
 		<button type="submit">Sign In</button>
 	</form>
 	<br />
 
 	<h1>Sign Up</h1>
-	<form on:submit={signUp}>
+	<form>
 		<label for="name">Name</label>
-		<input type="text" bind:value={nameInput} required />
+		<input type="text" required />
 		<label for="emailInput">Email</label>
-		<input type="email" bind:value={emailInput} required />
+		<input type="email" required />
 		<label for="passwordInput">Password</label>
-		<input type="password" bind:value={passwordInput} required />
+		<input type="password" required />
 		<button type="submit">Sign Up</button>
 	</form>
 </main>
