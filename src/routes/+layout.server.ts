5a4547571da08cfa96c193d0b78d503ee954c720
tbollen <thomas.bollen97@gmail.com>
2025-10-16 09:35:04 +0200

    Refactoring for locals loaded from db

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,9 +3,8 @@ import { CardStore } from '$lib/core/cards/cardStore.svelte.js';
 
 export const load = async ({ locals }) => {
 	const cardsFromDb = await db.card.findMany();
-	const cardStore = new CardStore({ prisma: cardsFromDb });
 	return {
 		user: locals.user ?? null,
-		cards: cardsFromDb ?? []
+		dbCards: cardsFromDb ?? []
 	};
 };
