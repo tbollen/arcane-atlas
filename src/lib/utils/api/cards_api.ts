164e784b2c559d5a9f298866ba9a70728db7de57
tbollen <thomas.bollen97@gmail.com>
2025-10-16 12:20:17 +0200

    Added logtracing to API

--- src/lib/utils/api/cards_api.ts
+++ src/lib/utils/api/cards_api.ts
@@ -2,20 +2,29 @@
 const API_BASE = '/api/cards';
 import type { card as PrismaCard } from '@prisma/client';
 
+function logTrace(msg: string) {
+	console.debug(`CARD_API call: ${msg}`);
+	console.trace();
+}
+
 const CARD_API = {
 	async create(card: PrismaCard): Promise<{ success: boolean }> {
+		logTrace('create');
 		const res = await fetch(API_BASE, { method: 'POST', body: JSON.stringify(card) });
 		return await res.json();
 	},
 	async update(card: PrismaCard): Promise<{ success: boolean }> {
+		logTrace('update');
 		const res = await fetch(API_BASE, { method: 'PUT', body: JSON.stringify(card) });
 		return await res.json();
 	},
 	async delete(card: PrismaCard): Promise<{ success: boolean }> {
+		logTrace('delete');
 		const res = await fetch(API_BASE, { method: 'DELETE', body: JSON.stringify(card) });
 		return await res.json();
 	},
 	async get(): Promise<{ success: boolean }> {
+		logTrace('get');
 		const res = await fetch(API_BASE, { method: 'GET' });
 		return await res.json();
 	}
