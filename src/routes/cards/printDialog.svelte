0e5a4f0fa511dd6f8518826014bdccf028d8a631
tbollen <thomas.bollen97@gmail.com>
2025-10-17 14:16:52 +0200

    UI improvements to Print Dialog

--- src/routes/cards/printDialog.svelte
+++ src/routes/cards/printDialog.svelte
@@ -2,7 +2,6 @@
 	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import Icon from '@iconify/svelte';
-	import { goto } from '$app/navigation';
 </script>
 
 <Dialog.Root>
@@ -14,13 +13,16 @@
 			<Dialog.Title>Print Cards</Dialog.Title>
 			<Dialog.Description>Which format do you want to print?</Dialog.Description>
 		</Dialog.Header>
-		<ul class="list-inside list-disc">
+		<ul class="list-outside list-disc pl-4 text-sm">
 			<li>A4 will print 4 cards per page (4 front and 4 back sides)</li>
 			<li>Single with print all front and backsides as individual pages</li>
 		</ul>
 		<Dialog.Footer>
 			<Button href="/print?printMode=A4"><Icon icon="mdi:cards" />A4</Button>
 			<Button href="/print?printMode=single"><Icon icon="mdi:view-grid" />Single</Button>
+			<Dialog.Close class={buttonVariants({ variant: 'destructive' })}
+				><Icon icon="mdi:close" />Cancel</Dialog.Close
+			>
 		</Dialog.Footer>
 	</Dialog.Content>
 </Dialog.Root>
