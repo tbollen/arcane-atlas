5e479a680a62d213ccdd64871b20725d9c3d38de
tbollen <thomas.bollen97@gmail.com>
2025-10-20 20:53:32 +0200

    Update account page

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	// UI Component
 	import { Button } from '$lib/components/ui/button';
+	import * as Alert from '$lib/components/ui/alert';
 
 	// Navigation
 	import { goto, invalidateAll } from '$app/navigation';
 
 	import { authClient } from '$lib/utils/auth/auth-client';
+	import Icon from '@iconify/svelte';
 	interface Props {
 		data: any;
 	}
@@ -27,18 +29,41 @@
 	console.error('Data:', data);
 </script>
 
-{#await user}
-	<p>Loading...</p>
-{:then user}
-	{#if user}
-		<p>Logged in as {user.name}</p>
-		<p>Email: {user.email}</p>
-		<img src={user.image} alt={user.name} />
-		<br />
-		<Button variant="destructive" onclick={logOut}>Log Out</Button>
-	{:else}
-		<p>Not logged in</p>
-	{/if}
-{:catch error}
-	<p>Error: {error.message}</p>
-{/await}
+<main>
+	{#await user}
+		<p>Loading...</p>
+	{:then user}
+		{#if user}
+			<h1 class="text-3xl font-bold underline">Account</h1>
+
+			{#if !user.emailverified}
+				<Alert.Root variant="warn">
+					<Icon icon="mdi:alert" />
+					<Alert.Title>Verify your email</Alert.Title>
+					<Alert.Description>
+						<p>
+							Your email address has not been verified. Please check your inbox for a verification
+							or click resend to send the verification email again.
+						</p>
+						<Button variant="bold" onclick={() => alert('Not implemented yet')}>Resend</Button>
+					</Alert.Description>
+				</Alert.Root>
+			{/if}
+			<p>Logged in as {user.name}</p>
+			<p>Email: {user.email}</p>
+			<img src={user.image} alt={user.name} />
+			<br />
+			<div class="flex flex-row gap-2">
+				<Button variant="destructive" onclick={logOut}>Log Out</Button>
+				<Button
+					variant="destructive"
+					onclick={() => authClient.deleteUser({ callbackURL: '/login' })}>Delete Account</Button
+				>
+			</div>
+		{:else}
+			<p>Not logged in, redirecting to login</p>
+		{/if}
+	{:catch error}
+		<p>Error: {error.message}</p>
+	{/await}
+</main>
