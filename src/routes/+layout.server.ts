bf6197e8d21c6d9efa1c0c9dac5f63f4a30ae25c
tbollen <thomas.bollen97@gmail.com>
2025-10-23 12:54:15 +0200

    Add dummy "character" to set active character. Needs to be replaced later!!

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,6 +5,7 @@ export const load = async ({ locals }) => {
 	const cardsFromDb = await db.card.findMany();
 	return {
 		user: locals.user ?? null,
+		character: null,
 		dbCards: cardsFromDb ?? []
 	};
 };
