3918c8e721c20607d917cef1c562edd3e3e85043
tbollen <thomas.bollen97@gmail.com>
2025-10-24 12:55:44 +0200

    Added Typography styles

---
+++ src/lib/components/typography/title.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	let { children, class: className, ...restProps } = $props();
+</script>
+
+<h1
+	class={cn('text-balanced scroll-m-20 text-4xl font-extrabold tracking-tight', className)}
+	{...restProps}
+>
+	{@render children?.()}
+</h1>
