b4ff098bcb0cb6fbe71519985f04473ac22a0ada
tbollen <thomas.bollen97@gmail.com>
2025-10-24 14:32:00 +0200

    Improved Header component

--- src/lib/components/typography/header.svelte
+++ src/lib/components/typography/header.svelte
@@ -7,16 +7,17 @@
 			variant: {
 				// h1: 'text-balanced scroll-m-20 text-4xl font-extrabold tracking-tight',
 				h1: 'mt-12 scroll-m-20 text-4xl font-semibold tracking-tight transition-colors first:mt-0',
-				h2: 'mt-10 scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0',
-				h3: 'mt-8 scroll-m-20 text-2xl font-semibold tracking-tight',
-				h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
+				h2: 'mt-10 scroll-m-20 text-2xl font-semibold tracking-tight transition-colors first:mt-0',
+				h3: 'mt-8 scroll-m-20 text-lg font-semibold tracking-tight transition-colors first:mt-0',
+				h4: 'scroll-m-20 text-md font-semibold tracking-tight',
 				// SUBTEXTS
-				subtitle: 'text-muted-foreground text-xl leading-7 pb-4 inline-block'
+				subtitle: 'text-muted-foreground text-xl leading-7 mb-4 inline-block'
 			},
 			addition: {
 				indented: 'pl-6',
 				divider: 'border-b pb-2',
-				none: ''
+				none: '',
+				asFirst: 'mt-0'
 			}
 		},
 		defaultVariants: {
@@ -38,14 +39,16 @@
 		children,
 		variant,
 		addition = 'none',
+		tag,
 		class: className,
 		...restProps
 	}: WithElementRef<HTMLAttributes<HTMLHeadingElement>> & {
+		tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'p';
 		variant?: HeadingVariant;
 		addition?: HeadingType;
 	} = $props();
 
-	const htmlTag = variant == 'subtitle' ? variant : 'p';
+	const htmlTag = tag || (variant == 'subtitle' ? variant : 'p');
 </script>
 
 <svelte:element
