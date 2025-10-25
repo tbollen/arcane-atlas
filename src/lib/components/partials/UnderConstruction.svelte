6c77566ea24cab79d6726e8896912187c4dffa07
tbollen <thomas.bollen97@gmail.com>
2025-10-25 15:02:10 +0200

    Updated Under Construction component to allow for classes and children

--- src/lib/components/partials/UnderConstruction.svelte
+++ src/lib/components/partials/UnderConstruction.svelte
@@ -1,19 +1,42 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import type { WithElementRef } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	// Accept class and rest props
+	let {
+		ref = $bindable(null),
+		children,
+		class: className,
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLHeadingElement>> = $props();
 </script>
 
 <div class="w-full">
 	<div
-		class="relative overflow-hidden rounded-xl p-2 text-foreground"
+		class="relative overflow-hidden rounded-xl text-foreground"
 		aria-hidden="false"
 		role="status"
 		title="Under construction"
-		style="background: repeating-linear-gradient(135deg, #fbbf2455 0 18px, transparent 18px 36px);"
 	>
-		<div class="flex flex-row items-center justify-center gap-4 text-4xl">
+		<div class="constructionLines flex flex-row items-center justify-center gap-4 text-4xl">
 			<Icon icon="roentgen:crane-portal" />
 			<h1 class="text-xl">Under Construction</h1>
 			<Icon icon="mdi:wrench" />
 		</div>
+		{#if children}
+			<hr />
+			<div class="bg-obsidian-500/5 p-2">
+				{@render children?.()}
+			</div>
+			<hr class="constructionLines" />
+		{/if}
 	</div>
 </div>
+
+<style>
+	.constructionLines {
+		background: repeating-linear-gradient(135deg, #fbbf2455 0 18px, transparent 18px 36px);
+		padding: 0.5rem;
+	}
+</style>
