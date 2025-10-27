adddd90774b6ceb8f3e1bd79252456397a15b4d3
tbollen <thomas.bollen97@gmail.com>
2025-10-27 17:37:03 +0100

    Remove useless server log where it logs "account" (used as test)

--- src/routes/campaign/+page.server.ts
+++ src/routes/campaign/+page.server.ts
@@ -2,8 +2,6 @@ import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	console.log('account');
-
 	if (!locals.user) {
 		throw redirect(302, '/login');
 	}
