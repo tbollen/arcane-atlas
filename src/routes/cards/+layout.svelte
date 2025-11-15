f9e3f3a55d3b3e09b83da18c87743dc150b7ed0d
tbollen <thomas.bollen97@gmail.com>
2025-11-15 13:42:09 +0100

    HOTFIX: Added all card IDs to CardStore initialization

--- src/routes/cards/+layout.svelte
+++ src/routes/cards/+layout.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
 	// Needed types and classes for cardStore
 	import type { card as PrismaCard } from '@prisma/client';
-	import {
-		CardStore,
-		StoredCard,
-		CARD_CONTEXT_KEY,
-		type CardID
-	} from '$lib/domain/cards/cardStore.svelte';
-	import type { UserID } from '$lib/domain/users/user.js';
+	import { CardStore, StoredCard, CARD_CONTEXT_KEY } from '$lib/domain/cards/cardStore.svelte';
+	import type { UserID, CardID } from '$lib/domain/index.js';
 	import type { PrismaCardExtended } from '$lib/domain/cards/cardStore.svelte';
 	import type { User as PrismaUser } from '@prisma/client';
 
@@ -15,6 +10,7 @@
 	import { setContext } from 'svelte';
 
 	let { children, data } = $props();
+	const allCardIds: CardID[] = data.db_cards_ids as CardID[];
 
 	// INIT cards from data
 	let cards: StoredCard[] = data.db_cards.map((prismaCard) =>
@@ -26,7 +22,7 @@
 	);
 
 	// Init cardStore and set context
-	const db_cardStore = new CardStore({ cards }); //TODO: add localstorage fallback
+	const db_cardStore = new CardStore({ cards, ids: allCardIds }); //TODO: add localstorage fallback
 	setContext<CardStore>(CARD_CONTEXT_KEY, db_cardStore);
 </script>
 
