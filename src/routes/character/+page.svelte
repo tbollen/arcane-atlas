408d0becd519e4c00ddc4efeef0301a69a5999a3
tbollen <thomas.bollen97@gmail.com>
2025-11-15 15:25:25 +0100

    Intial tes page to add characters

--- src/routes/character/+page.svelte
+++ src/routes/character/+page.svelte
@@ -1,12 +1,41 @@
 <script lang="ts">
 	// UI Components
 	import { Header } from '$lib/components/typography/';
+	import { Button } from '$lib/components/ui/button/';
 	import UnderConstruction from '$lib/components/partials/UnderConstruction.svelte';
 	import '$lib/styles';
 
-	import { onMount } from 'svelte';
+	// Utils
+	import { onMount, getContext, setContext } from 'svelte';
+	import { ck, lsk } from '$lib/utils/storage/keys.js';
+	import { CharacterStore, StoredCharacter } from '$lib/domain/characters/character.svelte.js';
+	import type { UserID } from '$lib/domain/';
+
+	// Stores
+	import { activeCharacter } from '$lib/stores/activeCharacter.svelte.js';
+
+	const characterStore = getContext<CharacterStore>(ck.characterStore);
 
 	let { data } = $props();
+
+	// FUNCTIONS //
+
+	function addNewCharacter(template?: Partial<StoredCharacter>) {
+		if (!data.user) {
+			alert('You must be logged in to create a character.');
+			throw new Error('Client not logged in');
+		}
+		const userId = data.user.id as UserID;
+
+		const newCharacter = characterStore.addNew({ userId, data: template });
+		console.log('newCharacter', newCharacter);
+	}
+
+	function setActiveCharacter(character: StoredCharacter) {
+		activeCharacter.set(character.id); // Set active character
+		localStorage.setItem(lsk.activeCharacter, character.id);
+		console.log('Active Character set:', activeCharacter.id);
+	}
 
 	onMount(async () => {
 		// DO LATER
@@ -14,6 +43,22 @@
 </script>
 
 <main class="content">
+	<Button onclick={() => addNewCharacter()}>Add Character</Button>
+	<div class="mt-4 flex flex-col gap-2">
+		{#each characterStore.characters as character}
+			<Button
+				variant={activeCharacter.id == character.id ? 'blossom' : 'default'}
+				onclick={() => setActiveCharacter(character)}
+			>
+				<p>
+					{character.name} |
+					<code>{character.id}</code>
+				</p>
+			</Button>
+		{/each}
+	</div>
+
+	<!-- UNDER CONSTRUCTION PART -->
 	<Header variant="h1" class="mb-8">Character</Header>
 	<UnderConstruction class="h-36">
 		<Header variant="h3" tag="h2">Planned Features</Header>
