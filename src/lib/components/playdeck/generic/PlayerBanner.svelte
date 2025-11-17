91c525f3612b44540ddb5b21135b2a84a605b66d
tbollen <thomas.bollen97@gmail.com>
2025-11-17 07:44:55 +0100

    Created two dummy components to test configurations

---
+++ src/lib/components/playdeck/generic/PlayerBanner.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { type DeckProps } from '../types';
+
+	let { character, edit }: DeckProps = $props();
+</script>
+
+<div>
+	<img src={character.imageUrl} alt={character.name} />
+	<div>{character.name}</div>
+</div>
