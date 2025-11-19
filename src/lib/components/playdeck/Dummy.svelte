b3ff791e48dbabd4af35133cf2cf3a1bd1a73447
tbollen <thomas.bollen97@gmail.com>
2025-11-19 14:22:03 +0100

    Created a Dummy component to test playdeck

---
+++ src/lib/components/playdeck/Dummy.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { type WidgetProps } from './widget';
+	let { character, edit }: WidgetProps = $props();
+</script>
+
+<div class="flex h-full w-full items-center justify-center bg-blossom-500/30">DUMMY</div>
