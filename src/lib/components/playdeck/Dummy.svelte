e26a4c331c2266442e454533a0fac0497b9dfaf2
tbollen <thomas.bollen97@gmail.com>
2025-11-20 19:17:01 +0100

    Updated Dummy component to match required type

--- src/lib/components/playdeck/Dummy.svelte
+++ src/lib/components/playdeck/Dummy.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { type WidgetProps } from './widget';
-	let { character, edit }: WidgetProps = $props();
+	import { type WidgetComponentProps } from './widget';
+	let { character, edit }: WidgetComponentProps = $props();
 </script>
 
 <div class="flex h-full w-full items-center justify-center bg-blossom-500/30">DUMMY</div>
