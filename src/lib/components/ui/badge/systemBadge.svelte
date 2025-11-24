320d0384ac9a610ad831cbcce5f2111181efeac4
tbollen <thomas.bollen97@gmail.com>
2025-11-24 22:18:38 +0100

    Created new SystemBadge component, a shorthand for styling badges with systemKeys as input

---
+++ src/lib/components/ui/badge/systemBadge.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { ComponentProps } from 'svelte';
+	import { gameSystems, AR_KEY, GENERIC_KEY } from '$lib/gameSystems';
+	import Badge from './badge.svelte';
+
+	type System = keyof typeof gameSystems;
+
+	let {
+		ref = $bindable(null),
+		href,
+		class: className,
+		variant = 'default',
+		children,
+		gameSystem,
+		...restProps
+	}: ComponentProps<typeof Badge> & { gameSystem: System } = $props();
+
+	// TAILWIND CLASSES FOR SYSTEMS
+	const systemClasses: Record<System, string> = {
+		[AR_KEY]: 'bg-weave-500 text-white',
+		[GENERIC_KEY]: 'bg-slate-300 text-slate-950'
+	};
+
+	const tailwindClass = systemClasses[gameSystem] || systemClasses[GENERIC_KEY];
+</script>
+
+<Badge bind:ref {href} class="{tailwindClass} {className}" {...restProps}>
+	{@render children?.()}
+</Badge>
