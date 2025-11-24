99ab2da6ff7ea3c68eed8b5709d4528d00170362
tbollen <thomas.bollen97@gmail.com>
2025-11-24 22:35:56 +0100

    Added base tooltip component for displaying simple information

---
+++ src/lib/components/partials/InfoTooltip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+
+	let {
+		children,
+		icon = 'mdi:information'
+	}: {
+		children?: import('svelte').Snippet;
+		icon?: string;
+	} = $props();
+</script>
+
+<Tooltip.Provider>
+	<Tooltip.Root>
+		<Tooltip.Trigger>
+			<Icon {icon} />
+		</Tooltip.Trigger>
+		<Tooltip.Content>{children?.()}</Tooltip.Content>
+	</Tooltip.Root>
+</Tooltip.Provider>
