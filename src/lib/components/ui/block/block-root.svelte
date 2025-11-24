2982c4db5b999cf1d93786ca9ffd5467d823202c
tbollen <thomas.bollen97@gmail.com>
2025-11-24 22:13:51 +0100

    Added "Block" as a new wrapper component for Deck Widgets

---
+++ src/lib/components/ui/block/block-root.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let {
+		children,
+		borderBoxSize = $bindable()
+	}: {
+		children?: import('svelte').Snippet;
+		borderBoxSize?: ResizeObserverSize[];
+	} = $props();
+</script>
+
+<div bind:borderBoxSize class="flex h-full w-full flex-col gap-1 bg-obsidian-500/5">
+	{@render children?.()}
+</div>
