9e26e6a2381aa58d7d1d9bac1d1c96d0f7c50ee7
tbollen <thomas.bollen97@gmail.com>
2025-11-25 11:29:42 +0100

    Added basic info tooltip component to quickly show information

--- src/lib/components/partials/InfoTooltip.svelte
+++ src/lib/components/partials/InfoTooltip.svelte
@@ -2,20 +2,34 @@
 	import Icon from '@iconify/svelte';
 	import * as Tooltip from '$lib/components/ui/tooltip';
 
+	import type { HTMLAttributes } from 'svelte/elements';
+
 	let {
 		children,
-		icon = 'mdi:information'
+		icon = 'mdi:information',
+		class: className,
+		tooltipClass,
+		arrowClasses
 	}: {
 		children?: import('svelte').Snippet;
 		icon?: string;
+		class?: HTMLAttributes<HTMLDivElement>['class'];
+		tooltipClass?: HTMLAttributes<HTMLDivElement>['class'];
+		arrowClasses?: HTMLAttributes<HTMLDivElement>['class'];
 	} = $props();
 </script>
 
-<Tooltip.Provider>
-	<Tooltip.Root>
-		<Tooltip.Trigger>
-			<Icon {icon} />
-		</Tooltip.Trigger>
-		<Tooltip.Content>{children?.()}</Tooltip.Content>
-	</Tooltip.Root>
-</Tooltip.Provider>
+{#if children && children.length > 0}
+	<Tooltip.Provider>
+		<Tooltip.Root>
+			<Tooltip.Trigger class="cursor-help {className}">
+				<Icon {icon} />
+			</Tooltip.Trigger>
+			<Tooltip.Content
+				class={tooltipClass}
+				arrowClasses="{arrowClasses?.toString()} {tooltipClass?.toString()}"
+				><div>{@render children()}</div></Tooltip.Content
+			>
+		</Tooltip.Root>
+	</Tooltip.Provider>
+{/if}
