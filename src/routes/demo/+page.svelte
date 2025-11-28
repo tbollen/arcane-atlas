9fd1cbb56522f96d41694f05eefd1f16e0912847
tbollen <thomas.bollen97@gmail.com>
2025-11-28 13:06:00 +0100

    Demo page: test with Dice Icons

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,80 +1,13 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-
-	class NestedClass {
-		text: string = 'this is a nested string';
-
-		setString(str: string) {
-			this.text = str;
-		}
-	}
-
-	class Test {
-		name: string = $state('test');
-		nest = $state(new NestedClass());
-		test: string = $state('');
-		serialized: string = $state('');
-		updatedTimestamp: Date = new Date();
-
-		serialize() {
-			const _serialized = {
-				...this
-			};
-			this.serialized = JSON.stringify(_serialized);
-			return _serialized;
-		}
-
-		setTest(str: string) {
-			this.test = str;
-		}
-
-		update() {
-			this.updatedTimestamp = new Date();
-			this.serialize();
-		}
-
-		constructor() {
-			this.serialize();
-		}
-	}
-
-	let test = new Test();
-
-	let serialized: string = $state('');
-
-	let derivedClassInstance = $derived(test);
-	let derivedString = $derived(derivedClassInstance.nest.text);
+	import DiceIcon from '$lib/components/partials/arcaneRift/DiceIcon.svelte';
+	import { genesysDiceSymbolsMap } from '$lib/components/partials/arcaneRift/DiceIcon.svelte';
 </script>
 
-<h1 class="text-3xl">This is my TEST</h1>
-<p>Test name: {test.name}</p>
-<hr class="my-4" />
-<p>Nested string: {test.nest.text}</p>
-<Button
-	onclick={() => {
-		test.nest.setString(Math.random().toString());
-	}}>Set Nested String (random number)</Button
->
-<Button
-	onclick={() => {
-		test.nest.text = 'Override';
-	}}>Override Nested String</Button
->
-<hr class="my-4" />
-{#if false}
-	<p>Top-level string: {test.test}</p>
-{/if}
-<Button
-	onclick={() => {
-		test.setTest(Math.random().toString());
-	}}>Set Top-level String (random number)</Button
->
-<hr class="my-4" />
-<Button variant="advanced" onclick={() => test.update()}>Update</Button>
-<hr class="my-4" />
-<Button
-	onclick={() => {
-		console.log(test.serialize());
-	}}>Serialize</Button
->
-<p>Serialized: {test.updatedTimestamp}</p>
+<div class="mx-auto my-2 flex w-2xl flex-wrap justify-between">
+	{#each Object.keys(genesysDiceSymbolsMap) as _symbol}
+		{@const symbol = _symbol as keyof typeof genesysDiceSymbolsMap}
+		<div class="block p-4">
+			<DiceIcon {symbol} class="text-4xl" richColor />
+		</div>
+	{/each}
+</div>
