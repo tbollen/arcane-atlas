bafd8fded9f075008af177e09164297bf263cbf5
tbollen <thomas.bollen97@gmail.com>
2025-11-29 13:44:02 +0100

    Added various variable layouts to the Stats widget

--- src/lib/components/playdeck/arcaneRift/Stats.svelte
+++ src/lib/components/playdeck/arcaneRift/Stats.svelte
@@ -8,6 +8,7 @@
 	import Mastery from '$lib/components/partials/arcaneRift/Mastery.svelte';
 	import InfoTooltip from '$lib/components/partials/InfoTooltip.svelte';
 	import * as Tooltip from '$lib/components/ui/tooltip';
+	import * as Dialog from '$lib/components/ui/dialog';
 
 	import { toast } from 'svelte-sonner';
 
@@ -16,7 +17,6 @@
 
 	import {
 		arcaneRiftDefaultCharacterRules,
-		type Consequence,
 		type Skill,
 		type Characteristic
 	} from '$lib/gameSystems/ArcaneRift/ar_characters';
@@ -41,63 +41,136 @@
 	let height: number = $derived(borderBoxSize?.[0]?.blockSize ?? 0);
 	let width: number = $derived(borderBoxSize?.[0]?.inlineSize ?? 0);
 
-	let limitedList = $derived(height < 200);
-	let compactList = $derived(height < 300);
+	let singleRow = $derived(height < 200);
+
+	// Get the amount of characters on the largest "skill" name in the character
+	let longestSkillName: string = $derived.by(() => {
+		if (!skills) return '';
+
+		let longest = '';
+		for (const skill of Object.values(skills)) {
+			if (skill.name.length > longest.length) {
+				longest = skill.name;
+			}
+		}
+		return longest;
+	});
+
+	let estimatedSkillNameWidth: string = $derived(
+		`${Math.max(16, longestSkillName.length * 0.5 + 1)}em` // Approx. 0.5em per character + padding
+	);
+
+	// DIALOG
+	let dialogOpen = $state(false);
 </script>
+
+<!-- SNIPPET FOR VALUE BOXES -->
+{#snippet numBox(num: string | number, css: string = 'h-full aspect-square')}
+	<div class="{css} flex items-center justify-center bg-obsidian-200 text-2xl font-semibold">
+		{num}
+	</div>
+{/snippet}
 
 <!-- Safeguard to make sure the character has Arcane Rift mechanics -->
 {#if character.mechanics.hasOwnProperty(AR_KEY) && characteristics && skills}
-	<Block.Root bind:borderBoxSize>
-		<Block.Title title="Stats" />
-		<Block.Content>
-			<div id="Stats" class="grid grid-cols-[max-content_auto_1fr]">
-				{#each Object.values(characteristics) as char}
-					{@const skillAmount: number = Object.values(skills).filter((skill) => skill.characteristic == char.name).length}
-					<hr class=" col-span-full my-2 border-obsidian-500 first:border-t-0" />
-					<div class="col-span-full">
-						<Tooltip.Provider>
-							<Tooltip.Root>
-								<Tooltip.Trigger class="cursor-help">
-									<p class="font-semibold uppercase">{char.name}</p>
-								</Tooltip.Trigger>
-								<Tooltip.Content>{char.description}</Tooltip.Content>
-							</Tooltip.Root>
-						</Tooltip.Provider>
+	<!-- SNIPPET FOR CHARACTERISTIC DISPLAY /w TOOLTIP -->
+	{#snippet characteristic_sn(characteristic: Characteristic)}
+		<Tooltip.Provider>
+			<Tooltip.Root>
+				<Tooltip.Trigger class="cursor-help">
+					<p class="font-semibold uppercase">{characteristic.name}</p>
+				</Tooltip.Trigger>
+				<Tooltip.Content>{characteristic.description}</Tooltip.Content>
+			</Tooltip.Root>
+		</Tooltip.Provider>
+	{/snippet}
+
+	<!-- SNIPPET FOR SKILL DISPLAY /w TOOLTIP -->
+	{#snippet skill_sn(skill: Skill)}
+		<Tooltip.Provider>
+			<Tooltip.Root>
+				<Tooltip.Trigger class="cursor-help">
+					{skill.name}
+				</Tooltip.Trigger>
+				<Tooltip.Content
+					>{rules.stats.skills.find((s) => s.name == skill.name)?.description}</Tooltip.Content
+				>
+			</Tooltip.Root>
+		</Tooltip.Provider>
+	{/snippet}
+
+	<!-- SNIPPET FOR FULL-STATS -->
+	{#snippet fullStats()}
+		<div
+			id="Stats"
+			class="grid gap-x-8"
+			style="grid-template-columns: repeat(auto-fill,minmax({estimatedSkillNameWidth},1fr));"
+		>
+			{#each Object.values(characteristics) as char}
+				{@const skillAmount: number = Object.values(skills).filter((skill) => skill.characteristic == char.name).length}
+				<div class="grid grid-cols-[max-content_1fr_max-content] gap-x-3">
+					<hr class="col-span-3 my-2 border-obsidian-500 first:border-t-0" />
+					<div class="col-span-3">
+						{@render characteristic_sn(char)}
 					</div>
 					<div
-						class="col-span-1 mr-4 aspect-square h-full p-2"
+						class="col-span-1 mr-4 aspect-square h-full p-1"
 						style="grid-row: span {skillAmount} / span {skillAmount};"
 					>
-						<div
-							class="flex h-full w-full items-center justify-center bg-obsidian-200 text-2xl font-semibold"
-						>
-							{char.value}
-						</div>
+						{@render numBox(char.value)}
 					</div>
 					{#each Object.values(skills) as skill}
 						{#if skill.characteristic == char.name}
-							<p class="pr-6 text-sm">
-								<Tooltip.Provider>
-									<Tooltip.Root>
-										<Tooltip.Trigger class="cursor-help">
-											{skill.name}
-										</Tooltip.Trigger>
-										<Tooltip.Content
-											>{rules.stats.skills.find((s) => s.name == skill.name)
-												?.description}</Tooltip.Content
-										>
-									</Tooltip.Root>
-								</Tooltip.Provider>
+							<p class="overflow-hidden pr-6 text-sm text-ellipsis whitespace-nowrap">
+								{@render skill_sn(skill)}
 							</p>
 							<Mastery value={skill.value} max={rules.skills.maxMastery} />
 						{/if}
 					{/each}
-				{/each}
-			</div>
-			<br class="flex-grow" />
+				</div>
+			{/each}
+		</div>
+	{/snippet}
+
+	<!-- WIDGET BLOCK -->
+	<Block.Root bind:borderBoxSize>
+		<Block.Title title="Stats">
+			<Button variant="ghost" onclick={() => (dialogOpen = true)}>
+				<Icon icon="mdi:arrow-expand" class="text-lg" />
+			</Button>
+		</Block.Title>
+		<Block.Content>
+			{#if singleRow}
+				<div
+					class="grid h-full w-full grid-flow-col gap-6 overflow-x-auto px-2"
+					style="grid-auto-columns: minmax(max-content, 1fr);"
+				>
+					{#each Object.values(characteristics) as char}
+						<div class="flex flex-col items-center">
+							<p class="text-center text-sm">
+								{@render characteristic_sn(char)}
+							</p>
+							{@render numBox(char.value, 'aspect-square h-16')}
+						</div>
+					{/each}
+				</div>
+			{:else}
+				{@render fullStats()}
+				<br class="flex-grow" />
+			{/if}
 		</Block.Content>
 		<Block.Footer />
 	</Block.Root>
+
+	<!-- DIALOG -->
+	<Dialog.Root bind:open={dialogOpen}>
+		<Dialog.Content class="max-h-[95vh] min-w-fit overflow-y-auto">
+			<Dialog.Header>
+				<Dialog.Title>Character Stats</Dialog.Title>
+			</Dialog.Header>
+			{@render fullStats()}
+		</Dialog.Content>
+	</Dialog.Root>
 {:else}
 	<div class="flex h-full w-full flex-col items-center justify-center gap-4" id="{AR_KEY}:aspects">
 		<span class="text-muted-foreground">Arcane Rift not configured</span>
