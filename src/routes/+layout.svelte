b427c49cac691e8ffc669492fd1c179b76ed7d68
tbollen <thomas.bollen97@gmail.com>
2025-12-07 10:27:10 +0100

    Get viewport data from Layout and pass to Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script lang="ts" module>
+	export type WidthLayout = 'mobile' | 'narrow' | 'desktop';
+</script>
+
 <script lang="ts">
 	import '../app.css';
 
@@ -22,40 +26,44 @@
 
 	let { children, data } = $props();
 
+	// Layout
+	let viewportWidth: number = $state(0);
+	let mobileLayout: boolean = $derived(viewportWidth <= 640); // Tailwind 'sm' breakpoint
+	let narrowLayout: boolean = $derived(viewportWidth > 640 && viewportWidth <= 1024); // Tailwind 'md' breakpoint
+	let desktopLayout: boolean = $derived(viewportWidth > 1024); // Tailwind 'lg' breakpoint
+	let layout: WidthLayout = $derived(mobileLayout ? 'mobile' : narrowLayout ? 'narrow' : 'desktop');
+
+	let navbarBorderBoxSize: ResizeObserverSize[] = $state([]);
+	let navbarHeight: number = $derived(navbarBorderBoxSize[0]?.blockSize ?? 0);
+
+	onMount(() => {
+		const updateWidth = () => {
+			viewportWidth = window.innerWidth;
+		};
+
+		updateWidth();
+		window.addEventListener('resize', updateWidth);
+
+		return () => {
+			window.removeEventListener('resize', updateWidth);
+		};
+	});
+
 	onMount(() => {
 		spinner.complete(); // Always remove loading spinner when page mounts
-
-		// // Set active character in Context
-		// // If user is logged in
-		// if (data.user) {
-		// 	const user = data.user as PrismaUser;
-		// 	// Format all user's characters
-		// 	const clientCharacters: StoredCharacter[] = data.characters.map((c) =>
-		// 		StoredCharacter.fromPrisma({ character: c, user })
-		// 	);
-		// 	// Get active character from localStorage (perisitent)
-		// 	const activeCharacterID = localStorage.getItem(lsk.activeCharacter);
-
-		// 	// Set active character
-		// 	const activeCharacter: StoredCharacter | undefined = clientCharacters.find(
-		// 		(c) => c.id === activeCharacterID
-		// 	);
-
-		// 	// Set in Context
-		// 	setContext<StoredCharacter | undefined>(ck.activeCharacter, activeCharacter);
-		// 	console.log('Active Character:', activeCharacter);
-		// }
 	});
 </script>
 
 <!-- TOASTER -->
 <Toaster richColors />
 
+<!-- NAVBAR -->
+<Navbar {data} {layout} bind:borderBoxSize={navbarBorderBoxSize} />
 <!-- CONTENT -->
-<header class="sticky top-0 z-1 print:hidden">
-	<Navbar {data} />
-</header>
-{@render children?.()}
+<section class="flex-grow">
+	{@render children?.()}
+</section>
+<!-- FULL PAGE SPINNER -->
 {#if spinner.isLoading && spinner.id === 'full'}
 	<div
 		class="absolute inset-0 z-50 flex flex-col items-center justify-center gap-3.5 bg-primary/80"
@@ -67,6 +75,14 @@
 		{/if}
 	</div>
 {/if}
+<!-- FOOTER -->
+<footer
+	class="border-t-2 border-threat-500 bg-obsidian-50 px-4 py-2
+	text-center print:hidden"
+	style="margin-top: {navbarHeight}px;"
+>
+	<p class="text-sm text-muted-foreground">© 2025 Arcane Atlas. All rights reserved.</p>
+</footer>
 
 <style>
 	:global(main section) {
@@ -75,5 +91,9 @@
 
 	:global(body) {
 		margin: 0;
+		position: relative;
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
 	}
 </style>
