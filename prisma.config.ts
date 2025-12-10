68403e038ed643eaa8725d777f68b4ad7606de2a
tbollen <thomas.bollen97@gmail.com>
2025-12-10 12:50:47 +0100

    Set migrations folder

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,5 +3,6 @@ import 'dotenv/config';
 
 export default {
 	// This is where you define your database connection.
-	schema: './prisma/schema/'
+	schema: './prisma/schema/',
+	migrations: './prisma/migrations/'
 };
